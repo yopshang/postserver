ea23dd5de1cfc31e21031d6ce15bffaa93f949ed
Shang <yopshang@gmail.com>
2022-05-21 14:42:44 +0800

    取得使用者資料

--- routes/users.js
+++ routes/users.js
@@ -6,20 +6,12 @@ const get_my_user = require('../controllers/get_my_user');
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   try{
-    const id = req.params.id;
+    const id = req.query.id;
     console.log('user id:', id);
     get_my_user(req, res, id);
   }catch(err){
     console.log(err);
   }
 });
-// router.get('/myfollows/:id', async function(req, res, next) {
-//   try{
-//     const id = await req.params.id;
-//     get_myfollows(res, req, id);
-//   }catch(err){
-//     console.log(err);
-//   }
-// });
 
 module.exports = router;
