a847895eecafd599988cdbd98eb51ff8606a2baf
Shang <yopshang@gmail.com>
2022-05-22 22:38:56 +0800

    移除多的code

--- routes/users.js
+++ routes/users.js
@@ -5,13 +5,9 @@ const get_my_user = require('../controllers/get_my_user');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  try{
     const id = req.query.id;
     console.log('user id:', id);
     get_my_user(req, res, id);
-  }catch(err){
-    console.log(err);
-  }
 });
 
 module.exports = router;
