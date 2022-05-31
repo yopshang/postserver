3daf9448fa4fa7ea3c61d3e2c20bd710de9cb2b2
Shang <yopshang@gmail.com>
2022-05-31 19:47:52 +0800

    get post add middleware

--- routes/posts.js
+++ routes/posts.js
@@ -5,17 +5,13 @@ const get_all_post = require('../controllers/get_all_post');
 const postController = require('../controllers/postpost');
 
 const appError = require('../error/appError');
+const checkId = require('../middleware/checkId');
+const checkPage = require('../middleware/checkPage');
 
-router.get('/', function(req, res, next){
-    if(req.query.page == undefined || req.query == 0){
-        return appError(400, '找不到該頁資料', next);
-    } else if (req.query.id == undefined || req.query.id == ''){
-        return appError(400, '找不到此id', next);
-    } else{
-        const page = req.query.page;
-        const id = req.query.id;
-        get_all_post(req, res, page, id);
-    }
+router.get('/', checkId, checkPage, function(req, res, next){
+    const page = req.query.page;
+    const id = req.query.id;
+    get_all_post(req, res, page, id);
 })
 router.post('/', function(req, res, next){
     if(req.query.id == undefined || req.query.id == ''){
