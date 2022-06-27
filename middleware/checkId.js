7acf3af80846a86c76f23859925e15c4cc2a5bc4
Shang <yopshang@gmail.com>
2022-06-27 13:24:54 +0800

    id驗證middleware

--- middleware/checkId.js
+++ middleware/checkId.js
@@ -1,8 +1,11 @@
+const mongoose = require('mongoose');
 const appError = require('../error/appError');
 
 const checkId = function(req, res, next){
     if(req.query.id == undefined || req.query.id.length == 0){
         return appError(400, "你沒有填寫id", next);
+    } else if(mongoose.isObjectIdOrHexString(req.query.id)){
+        return appError(400, "id格式錯誤", next);
     } else {
         next();
     }
