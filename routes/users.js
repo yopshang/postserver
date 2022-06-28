cb2d20bbb7eed3ecb371d6e826b967169bee8b08
Shang <yopshang@gmail.com>
2022-06-28 21:38:36 +0800

    add check email

--- routes/users.js
+++ routes/users.js
@@ -1,16 +1,20 @@
 var  express = require('express');
 var router = express.Router();
 
+// controller
 const get_my_user = require('../controllers/get_my_user');
 const handleErrorAsync = require('../error/handErrorAsync');
-const checkId = require('../middleware/checkId');
 const userController = require('../controllers/user');
+
+// middeleware
+const checkId = require('../middleware/checkId');
+const checkEmail = require('../middleware/checkEmail');
 
 router.get('/',checkId , async function(req, res, next) {
     const id = req.query.id;
     get_my_user(req, res, id);
 });
-router.post('/add_user', (req, res, next)=>{
+router.post('/add_user', checkEmail, (req, res, next)=>{
     handleErrorAsync(userController.add_user(req, res, next))
 });
 
