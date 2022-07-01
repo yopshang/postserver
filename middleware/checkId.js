385451bd7451017f62786ae8241afb072bcf5e23
Shang <yopshang@gmail.com>
2022-07-01 08:25:10 +0800

    新增 isObjectIdOrHexString id 判定

--- middleware/checkId.js
+++ middleware/checkId.js
@@ -4,11 +4,9 @@ const appError = require('../service/appError');
 const checkId = function(req, res, next){
     if(req.body.id == undefined || req.body.id.length == 0){
         return appError(400, "你沒有填寫id", next);
-    } 
-    // else if(mongoose.isObjectIdOrHexString(req.body.id)){
-    //     return appError(400, "id格式錯誤", next);
-    // } 
-    else {
+    } else if(!mongoose.isObjectIdOrHexString(req.body.id)){
+        return appError(400, "id格式錯誤", next);
+    } else {
         next();
     }
 }
