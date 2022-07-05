a325b8818dd4bdd3f5d60c495b609c80c6cb86e3
Shang <yopshang@gmail.com>
2022-07-05 21:09:56 +0800

    validator email 驗證

--- middleware/checkEmail.js
+++ middleware/checkEmail.js
@@ -1,6 +1,6 @@
 const failHandler = require('../service/failHandler');
 const successHandler = require('../service/successHandler');
-// const validator = require('validator');
+const validator = require('validator');
 const userModal = require('../models/user');
 const e = require('express');
 
@@ -8,6 +8,8 @@ const checkEmail = async function(req, res, next){
     const ifExecuted = await userModal.countDocuments({email: req.body.email});
     if(ifExecuted>0){
         failHandler(res, '400', 'email已存在')
+    } else if(!validator.isEmail(req.body.email)){
+        failHandler(res, '400', 'email格式不符')
     } else {
         next()
     }
