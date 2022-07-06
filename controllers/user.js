dcf063ffb40a8000412573d13649528b27d93ae4
EZPAY_OA\shang.huang <shang.huang@ezpay.com.tw>
2022-07-06 15:07:30 +0800

    簽發token 與 驗證登入建立

--- controllers/user.js
+++ controllers/user.js
@@ -2,15 +2,27 @@
 const userModel = require('../models/user');
 // service
 const successHandler = require('../service/successHandler')
+const failHandler = require('../service/failHandler')
+const bycript = require('bycript');
+// const generateJWT = require('../controllers/generateJWT');
+const {isAuth, generateJWT} = require('../service/isAuth');
 
 const userController = {
     sign_in:async function (req, res){
-            const users = await userModel.find({email:req.body.email});
-            // const token = await jwt.
-            res.status(200).json({
-                status: 'success',
-                token: token
-            });
+            const user = await userModel.find({email:req.body.email});
+            const ifMatch = await bycript.compare(users.password, res); // 符合就會給true
+            if(ifMatch){
+                // 回傳token
+                // const token = await jwt.sign({id:users.id}, process.env.JWT_SECRET, {})
+                // successHandler(res, 200, '登入成功', token)
+                generateJWT(user, 200, res);
+            } else {
+                failHandler(res, 400, '密碼或是email錯誤')
+            }
+            // res.status(200).json({
+            //     status: 'success',
+            //     token: token
+            // });
     },
     sign_up: async function(req, res){
         // const password =  await bcrypt.hash(req.body.password, 12);
---
+++ service/isAuth.js
@@ -0,0 +1,50 @@
+const jwt = require('jsonwebtoken');
+const handErrorAsync = require('../service/handErrorAsync')
+const appError = require('../service/appError');
+const userController = require('../controllers/user');
+
+const isAuth = handErrorAsync(async (req, res, next) => { // 驗證登入會以middleware使用 
+  // 確認token是否存在
+  let token;
+  if(res.headers.authorization && req.headers.authorization.startWith('Auth')) {// 確認是否token用Auth
+    token = req.headers.authorization.split('')[1]; // 取得token後拿第[1]個, 有取到代表有登入，沒取到代表沒登入
+  }
+  if(!token){ // token不存在代表未登入 next到錯誤回報
+    return next(appError(401, '您尚未登入!', next));
+  }
+  // 建立一個promise用來驗證token正確性
+  const decode = await new Promise((resolve, reject) => {
+    jwt.verify(token, process.env.JWT_SECRET, (err, payload)=>{
+      if(err){
+        reject(err)
+      } else {
+        resolve(payload)
+      }
+    })
+    const currentUser = await userController.findById(decode.id); // 使用解碼出來的id去db撈目前使用者帳號
+
+    req.user = currentUser; // 字定義req中的User為目前撈到的user
+    next();
+  })
+})
+
+const generateJWT = (user, statusCode, res)=>{
+  // 產生JWT token
+  // 使用id去簽發一個token
+  const token = jwt.sign({id:user._id}, process.env.JWT_SECRET,{
+    expiresIn: process.env.JWT_EXPIRES_DAY // 設定token到期天數
+  })
+  user.password = undefined; // 設為undefined? 不要讓前台看到?
+  res.status(statusCode).json({ // 回傳東西給前台
+    status: 'success',
+    user: {
+      token,
+      user: user.name
+    }
+  })
+}
+
+module.exports = { // 驗證登入與簽發jwt 個別匯出
+  isAuth,
+  generateJWT
+}
